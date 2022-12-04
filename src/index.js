750c9a73a123eba2b111311eb88d17a8305b9e82
Jiglau Mihai <mihai.jiglau@hpe.com>
2022-12-04 19:56:45 +0200

    Added Roboto Font

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import '@fontsource/roboto';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
