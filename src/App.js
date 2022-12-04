c0a3ddbb1776ddd1e97ab62d46d11b35786bb377
Jiglau Mihai <mihai.jiglau@hpe.com>
2022-12-04 19:55:54 +0200

    Added SimpleForm component and background color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import SimpleForm from "./components/SimpleForm";
 
 export default function App () {
     return (
-      <div>
-
+      <div className="App">
+        <SimpleForm />
       </div>
     );
 }
